da02a8874a926f12d44a579a9cccc5cada609641
Mayank Singh Chauhan <mayanksingh2298@gmail.com>
2017-06-11 16:30:36 +0530

    Image Stegano ready

--- py_img_stegano/decrypt_hlbt.py
+++ py_img_stegano/decrypt_hlbt.py
@@ -1,23 +1,38 @@
 from breezypythongui import EasyFrame
+from breezypythongui import EasyDialog
 from math import log
 from tkFileDialog import askopenfilename
 from PIL import Image
+from Tkinter import Entry,StringVar
+from random import randint
+
+#in YES store some questions whose answer is a yes
+YES=["Does the sun rise in the east?","Do two and two make four?","Is water a five lettered word?","Are there twenty four hours in a day?","Can sunlight generate energy?"]
+#in NO store some questions whose answer is a no
+NO=["Do we humans have sixteen hands?","Is light a three lettered word?","Is b the first letter of english alphabets?","Do all trees hate sunlight?","Can we drink fire?"]
+
+
 class Decryptor(EasyFrame):
 	def __init__(self):
-		EasyFrame.__init__(self,title='Decryptor',width = 600,height=300,resizable=False)
+		EasyFrame.__init__(self,title='Decryptor',width = 1200,height=300,resizable=False)
 		self.addLabel(text = "Welcome to the Decryptor", row = 0, column = 0, columnspan = 2)
 		self.addLabel(text="Decrypted message : ", row=1, column=0)
 		self.message=self.addLabel(text='',row=1,column=1)
 		self.addLabel(text="Enter the Password : ", row=2, column=0)
-		self.Password=self.addTextField('',row=2,column=0, width=20)
+		#self.Password=self.addTextField('',row=2,column=0, width=20)
+
+		self.Password = StringVar() #Password variable
+		passEntry = Entry(self, textvariable=self.Password, show='*').grid(row=2,column=1) 
+		
 		self.addLabel(text="Select the Image : ", row=3, column=0)
 		self.filename=self.addLabel(text="", row=3, column=1)
 		self.browse=self.addButton(text="Browse",row=4, column=1,command=self.Browse)
 		self.decrypt=self.addButton(text='Decrypt',row=5,column=0,state='disabled',command=self.Decrypt)
 		self.hint=self.addLabel(text="Awaiting input.......", row=5,column=1)
+		self.captcha_ans=["0"]
 
 	def Browse(self):
-		filetypes = [("Image files", "*.png")]
+		filetypes = [("Image files", "*.png"),("Image files", "*.bmp"),]
         	fileName =askopenfilename(parent = self,filetypes = filetypes)
 		self.filename['text']=fileName
 		self.hint['text']='Awaiting input.......'
@@ -25,8 +40,13 @@ class Decryptor(EasyFrame):
 		self.message['text']=''
 
 	def Decrypt(self):
+		dialog = Captcha(self, self.captcha_ans)
+		if dialog.modified()==False or self.captcha_ans[0]=="0":
+			self.message['text']="error in captcha"
+			return
 		self.decrypt['state']='disabled'
-		password = self.Password.getText()
+		#password = self.Password.getText()
+		password=self.Password.get()
 		#write the code for image processing#######################
 
 		im=Image.open(self.filename['text'])
@@ -102,8 +122,66 @@ class Decryptor(EasyFrame):
 			mess+=chr(ascii_int)
 
 		###########################################################		
-		self.hint['text']='Image file decrypted'
+		self.hint['text']='Program status: Done'
 		self.filename['text']=''
 		self.message['text']=mess
+		self.captcha_ans=["0"]
+
+#For captcha Dialog Box
+class Captcha(EasyDialog):
+	
+	"""Opens a dialog for captcha"""
+	def __init__(self, parent,captcha_ans):
+		"""Sets up the window."""
+		self.gamble=randint(0,1)		
+		self.captcha_ans=captcha_ans
+		EasyDialog.__init__(self, parent, "Captcha")
+    
+	def body(self, master):
+		#self.question=self.addLabel(master,text = "Select Red", row = 0, column = 0)
+		ques=""
+		if self.gamble==0:
+			ques="No"
+		else:
+			ques="Yes"
+		self.addLabel(master,text = "Select the question whose answer is "+ques, row = 0, column = 0)
+		# Add the button group
+		self.correct_pos=randint(0,len(YES)-1)
+		self.group = self.addRadiobuttonGroup(master,row = 1, column = 0, rowspan = len(YES))
+		if self.gamble==0:
+			for i in range(0,len(YES)):
+				if i==self.correct_pos:
+					self.group.addRadiobutton(NO[i])
+				else:
+					self.group.addRadiobutton(YES[i])
+		else:
+			for i in range(0,len(YES)):
+				if i==self.correct_pos:
+					self.group.addRadiobutton(YES[i])
+				else:
+					self.group.addRadiobutton(NO[i])
+
+
+
+
+
+		
+
+		# Add the radio buttons to the group
+		#self.group.addRadiobutton('Red')
+		#self.group.addRadiobutton("Blue")
+		#self.group.addRadiobutton("Yellow")
+		#self.group.addRadiobutton("Green")
+
+        
+
+	def apply(self):
+		"""Transfers data from the fields to the CD."""
+		selectedButton = self.group.getSelectedButton()
+		if (self.gamble==0 and selectedButton["text"]==NO[self.correct_pos]) or (self.gamble==1 and selectedButton["text"]==YES[self.correct_pos]) :
+			self.captcha_ans[0]="1"
+		self.setModified()
+
+
 if __name__ == "__main__":
 	Decryptor().mainloop()
--- py_img_stegano/encrypt_hlbt.py
+++ py_img_stegano/encrypt_hlbt.py
@@ -3,22 +3,31 @@ from math import log
 from tkFileDialog import askopenfilename
 from PIL import Image
 from os.path import basename
+from Tkinter import Entry,StringVar
 class Encryptor(EasyFrame):
 	def __init__(self):
 		EasyFrame.__init__(self,title='Encryptor',width = 600,height=300,resizable=False)
 		self.addLabel(text = "Welcome to the Encryptor", row = 0, column = 0, columnspan = 2)
 		self.addLabel(text = "Enter the message : ", row=1, column=0)
 		self.message=self.addTextArea('',row=1,column=1,height=2, width=20,wrap='word')
-                self.addLabel(text="Enter the Password : ", row=2, column=0)
-		self.Password=self.addTextField('',row=2,column=0, width=20)
+                self.addLabel(text="Enter the Password : (max size is 10)", row=2, column=0)
+		#self.Password=self.addTextField('',row=2,column=0, width=20)
+		
+		self.Password = StringVar() #Password variable
+		passEntry = Entry(self, textvariable=self.Password, show='*').grid(row=2,column=1)
+		#self.Password.set(self.Password.get()[:10])
+
 		self.addLabel(text="Select the Image : ", row=3, column=0)
 		self.filename=self.addLabel(text="", row=3, column=1)
 		self.browse=self.addButton(text="Browse",row=4, column=1,command=self.Browse)
 		self.encrypt=self.addButton(text='Encrypt',row=5,column=0,state='disabled',command=self.Encrypt)
 		self.hint=self.addLabel(text="Awaiting input.......", row=5,column=1)
 
+		
+			
+
 	def Browse(self):
-		filetypes = [("Image files", "*.png")]
+		filetypes = [("Image files", "*.png"),("Image files", "*.bmp")]
         	fileName =askopenfilename(parent = self,filetypes = filetypes)
 		self.filename['text']=fileName
 		self.hint['text']='Awaiting input.......'
@@ -27,11 +36,16 @@ class Encryptor(EasyFrame):
 	def Encrypt(self):
 		self.encrypt['state']='disabled'
 		#write the code for image processing
-		self.hint['text']='Image file generated'
+		
 		
 		image_path=self.filename['text']
 		message=self.message.getText()
-		password = self.Password.getText()
+		#password = self.Password.getText()
+		password=self.Password.get()
+		if len(password)>10:
+			self.messageBox("ERROR ALERT", "The max password size is 10")
+			self.encrypt['state']='normal'
+			return
 ###############################################################
 	
 		""" assume that image size is sufficient"""
@@ -39,6 +53,10 @@ class Encryptor(EasyFrame):
 
 		pix=im.load()
 		width,height=im.size
+		if len(message)>(width*height)/3:
+			self.messageBox("ERROR ALERT", "You have exceeded the maximum message size supported by the current image, which is "+str((width*height)/3))
+			self.encrypt['state']='normal'
+			return
 		key = 0		
 		for i in range(len(password)):
 			ch=password[i]
@@ -119,6 +137,7 @@ class Encryptor(EasyFrame):
 					x,y = traverse2(9*index+3*i+j+1 + key)
 		s=basename(image_path)
 		im.save('en'+s)
+		self.hint['text']='Image file generated'
 ###############################################################
 		self.filename['text']=''
 if __name__ == "__main__":
